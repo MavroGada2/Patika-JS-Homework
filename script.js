4c7bd80a0c02b1c5eba7eb6b532ef6444dabd0b0
Ekin Mazlum <103841664+MavroGada2@users.noreply.github.com>
2023-02-06 18:02:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let firstName = prompt("isminizi giriniz:")
+
 if(firstName){
     document.querySelector("#myName").innerHTML = firstName
-function tarihSaat() {
-    let date = new Date().toLocaleString('tr-tr',{ weekday:"long", month:"long", day:"numeric", hour:"numeric", minute:"numeric", second:"numeric"});
-    document.querySelector("myClock");
-    myClock.innerHTML = date;
-}
+    function tarihSaat() {
+         let date = new Date().toLocaleString('tr-tr',{ weekday:"long", month:"long", day:"numeric", hour:"numeric", minute:"numeric", second:"numeric"});
+         document.querySelector("myClock");
+         myClock.innerHTML = date;
+    }
 // her 1 saniyede tarihSaat fonksiyonunu yeniden çalıştır
 setInterval(tarihSaat, 1000); 
+
+}
