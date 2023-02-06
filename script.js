c7cd0706ec021a86a162de91e7dab196bd4cb5a8
Ekin Mazlum <103841664+MavroGada2@users.noreply.github.com>
2023-02-06 17:57:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let firstName = prompt("isminizi giriniz:")
+if(firstName){
+    document.querySelector("#myName").innerHTML = firstName
 function tarihSaat() {
     let date = new Date().toLocaleString('tr-tr',{ weekday:"long", month:"long", day:"numeric", hour:"numeric", minute:"numeric", second:"numeric"});
     document.querySelector("myClock");
